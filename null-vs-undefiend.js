80b475d88295b133065f4ce273b611cb8af8f5d0
SHOPON <shoponkhan212020@gmail.com>
2021-02-05 17:41:43 +0600

    add all interview question

--- null-vs-undefiend.js
+++ null-vs-undefiend.js
@@ -1,17 +1,20 @@
-// let pakhi;
+let pakhi;
 
-// function add(num1, num2){
-//     console.log(num1 + num2)
-// }
+function add(num1, num2){
+    console.log(num1 + num2)
+}
 
-// function add(num1, num2){
-//     console.log(num1,num2)
-// }
+function add(num1, num2){
+    console.log(num1,num2)
+}
 
-// const premik = {name:"amirkhan", phone: "0175555"};
-// console.log(premik.gf);
+const premik = {name:"amirkhan", phone: "0175555"};
+console.log(premik.gf);
 
 let fun = undefined;
 
 let ages = [2, 3, 9];
 console.log(ages[11]);
+
+// Null use hobe jokon kuno variabele declear kora hobe na
+//  aber jokon kuno akte variable take kuno kesu paite chi ja sakane nai tokon undefiend show kore
