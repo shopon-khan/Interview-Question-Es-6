ec355a06062b9193b84195405aed464ee4ff239d
SHOPON <shoponkhan212020@gmail.com>
2021-02-05 20:50:57 +0600

    now learn map and filter

---
+++ map-filter.js
@@ -0,0 +1,63 @@
+// const numbers = [3, 5, 6, 7, 8];
+// const output = [];
+
+// for (let i = 0; i< numbers.length; i++) {
+//   const element = numbers[i];
+//   const result = element * element;
+//   output.push(result);
+
+//     }
+
+// function square (element){
+//     return element * element;
+// }
+
+// numbers.map(function(element,index,array){
+//     console.log(element, index, array);
+// })
+
+// function square (element){
+//     return element * element;
+// }
+
+// const square = element => element * element;
+// const square = x => x * x;
+
+//  const result = numbers.map(function(element){
+//    return element * element;
+// })
+
+
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+    
+//  filter can use for selected some variable
+// filter can use for condition full fill or if condition is not full fill he is ont show that elements if big and small
+
+// const numbers = [3, 5, 6, 7, 8];
+// const output = [];
+
+// const bigger = numbers.filter(x => x > 5);
+// console.log(bigger);
+    
+// const numbers = [3, 5, 6, 7, 8];
+// const output = [];
+
+// const bigger = numbers.filter(x => x < 5);
+// console.log(bigger);
+
+// filter jodi sorto poruna kawke na pai tahole akti empty array deya dan
+// const numbers = [3, 5, 6, 7, 8];
+// const output = [];
+
+// const bigger = numbers.filter(x => x < 3);
+// console.log(bigger);
+
+// fiend is kuno akti variable ar protom variable ta ke show korbe
+
+const numbers = [3, 5, 6, 7, 8];
+const output = [];
+
+const isThere = numbers.find(x => x > 5);
+console.log(isThere);
