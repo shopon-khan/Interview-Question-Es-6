bf2cca54751fcc35b921b971770122388f925737
SHOPON <shoponkhan212020@gmail.com>
2021-02-05 17:33:26 +0600

    null vs undefiend clear for interview

---
+++ null-vs-undefiend.js
@@ -0,0 +1,17 @@
+// let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2)
+// }
+
+// function add(num1, num2){
+//     console.log(num1,num2)
+// }
+
+// const premik = {name:"amirkhan", phone: "0175555"};
+// console.log(premik.gf);
+
+let fun = undefined;
+
+let ages = [2, 3, 9];
+console.log(ages[11]);
---
+++ truethy-false.js
@@ -0,0 +1,16 @@
+// const age = 4;
+let name = "salman kahn"
+
+if (name || name ==0) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false");
+}
+// false value 
+// false
+// 0
+// ""
+// undenfiend
+// null
+// turethy 
+// '0' ' ' {} []
