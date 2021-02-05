a81a94f7296a48b272fdd952b55a21843307c427
SHOPON <shoponkhan212020@gmail.com>
2021-02-05 18:20:44 +0600

    interview question for doble and tripel equel

---
+++ dobel-vs-triple-equal.js
@@ -0,0 +1,67 @@
+// equal not will be show false
+ 
+const first = 2;
+const second = 3;
+if (first == second) {
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+// if condition is equal will be show true
+const first = 2;
+const second = 2;
+if (first == second) {
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+// if condition is equal and string will be show true
+const first = 2;
+const second = "2";
+if (first == second) {
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+// when set === triple equal then show false because === equal identify what is it but == equal is ont  identify what is it?
+//  === equal check value and type & == equal check only value
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+// conpayer 1 value then show false 
+const first = 2;
+const second = true;
+if (first === second) {
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
+// when set == equal then show true
+const first = 0;
+const second = false;
+if (first == second) {
+    console.log("condition is true");
+
+}
+else{
+    console.log("condition is false");
+}
+
