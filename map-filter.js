a24aca5f271982fd2936c3d0b3b08935c8060532
SHOPON <shoponkhan212020@gmail.com>
2021-02-05 20:57:08 +0600

    add interview question map and find

--- map-filter.js
+++ map-filter.js
@@ -1,58 +1,58 @@
-// const numbers = [3, 5, 6, 7, 8];
-// const output = [];
+const numbers = [3, 5, 6, 7, 8];
+const output = [];
 
-// for (let i = 0; i< numbers.length; i++) {
-//   const element = numbers[i];
-//   const result = element * element;
-//   output.push(result);
+for (let i = 0; i< numbers.length; i++) {
+  const element = numbers[i];
+  const result = element * element;
+  output.push(result);
 
-//     }
+    }
 
-// function square (element){
-//     return element * element;
-// }
+function square (element){
+    return element * element;
+}
 
-// numbers.map(function(element,index,array){
-//     console.log(element, index, array);
-// })
+numbers.map(function(element,index,array){
+    console.log(element, index, array);
+})
 
-// function square (element){
-//     return element * element;
-// }
+function square (element){
+    return element * element;
+}
 
-// const square = element => element * element;
-// const square = x => x * x;
+const square = element => element * element;
+const square = x => x * x;
 
-//  const result = numbers.map(function(element){
-//    return element * element;
-// })
+ const result = numbers.map(function(element){
+   return element * element;
+})
 
 
 
-// const result = numbers.map(x => x * x);
-// console.log(result);
+const result = numbers.map(x => x * x);
+console.log(result);
     
 //  filter can use for selected some variable
 // filter can use for condition full fill or if condition is not full fill he is ont show that elements if big and small
 
-// const numbers = [3, 5, 6, 7, 8];
-// const output = [];
+const numbers = [3, 5, 6, 7, 8];
+const output = [];
 
-// const bigger = numbers.filter(x => x > 5);
-// console.log(bigger);
+const bigger = numbers.filter(x => x > 5);
+console.log(bigger);
     
-// const numbers = [3, 5, 6, 7, 8];
-// const output = [];
+const numbers = [3, 5, 6, 7, 8];
+const output = [];
 
-// const bigger = numbers.filter(x => x < 5);
-// console.log(bigger);
+const bigger = numbers.filter(x => x < 5);
+console.log(bigger);
 
 // filter jodi sorto poruna kawke na pai tahole akti empty array deya dan
-// const numbers = [3, 5, 6, 7, 8];
-// const output = [];
+const numbers = [3, 5, 6, 7, 8];
+const output = [];
 
-// const bigger = numbers.filter(x => x < 3);
-// console.log(bigger);
+const bigger = numbers.filter(x => x < 3);
+console.log(bigger);
 
 // fiend is kuno akti variable ar protom variable ta ke show korbe
 
